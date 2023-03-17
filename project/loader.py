a990f23ef81a6440a8668d3f54a023cdf145d771
Vitalii Haruk <dropharuk>
2023-03-17 21:18:10 +0200

    sdf

--- project/loader.py
+++ project/loader.py
@@ -1,17 +1,2 @@
-from modules.binance   import BinanceAPI
-from modules.clientokx import OkxAPI
-from modules.bybit     import BybitAPI
-from modules.coinbase  import CoinbaseAPI
-from modules.kucoinapi import KucoinAPI
-from modules.dydx      import DydxAPI
 
-
-
-# bestchangeapi = bestchangeAPI()
-binanceapi  = BinanceAPI()
-okxapi      = OkxAPI()
-bybitapi    = BybitAPI()
-coinbaseapi = CoinbaseAPI()
-kucoinapi   = KucoinAPI()
-dydxapi     = DydxAPI()
 
